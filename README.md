b56f55a9e59762df3fb03a4145917ec7bfbdf6c2
ziminkim <tender@9610@gmail.com>
2023-07-13 18:15:17 +0900

    new commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Iknome
+# Iknome
+# Iknome
---
+++ test/test/testApp.swift
@@ -0,0 +1,20 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 김지민 on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
